5fa4aeddd132432ad277c9197cbfc1cf08e17200
Sairam Venkataraman <SV00542797@techmahindra.com>
2018-02-19 19:17:00 +0530

    updated with changes to micro service - STS

---
+++ src/main/java/in/techmahindra/sts/anydomain/StsTempApplication.java
@@ -0,0 +1,12 @@
+package in.techmahindra.sts.anydomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsTempApplication.class, args);
+	}
+}
