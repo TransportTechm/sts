6708c167e8af3e06cd3b8a9f5eb2decf1341da90
Sairam Venkataraman <SV00542797@techmahindra.com>
2018-02-21 15:39:03 +0530

    updated with changes - end-points for sts

--- src/main/java/in/techmahindra/anydomain/sts/driver/StsApplication.java
+++ src/main/java/in/techmahindra/anydomain/sts/driver/StsApplication.java
@@ -2,8 +2,12 @@ package in.techmahindra.anydomain.sts.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import in.techmahindra.anydomain.sts.service.impl.CUserResourceImpl;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = CUserResourceImpl.class)
 public class StsApplication {
 
 	public static void main(String[] args) 
